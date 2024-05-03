54f4912e9d0458790bab8482db7ca4ccc5995bd3
Ashish1437 <121593481+Ashish1437@users.noreply.github.com>
2024-05-03 16:19:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6305751172:AAG3f0lLXE-ivB1r3xP0VSvUfR0nKfURYfM",
-             api_id= 29759544,
-             api_hash= "97030a871720998c6efc5debcce67aa4")
+             bot_token= "6859314703:AAEW3lEqUe2IL4uY41LLWUSRZ4VCUTn2g9s",
+             api_id= 25961801,
+             api_hash= "c9d39e5cba88255416aad52f0d0e1cd8")
 
 
 @bot.on_message(filters.command(["start"]))
@@ -200,7 +200,7 @@ async def account_login(bot: Client, m: Message):
 
             except Exception as e:
                 await m.reply_text(
-                    f"**downloading failed 🅰🅸🆁 🅿🅷🅴🅾🅽🅸🆇™**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}`"
+                    f"**downloading failed Perfect Smile ™**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}`"
                 )
                 continue
 
